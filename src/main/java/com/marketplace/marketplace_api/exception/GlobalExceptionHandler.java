6fb33e9b1866732673d1a9b1b14fe2a96386021b
Ebarhim Bennett <bennettebrahim04@gmail.com>
2026-04-10 19:59:22 +0200

    feat: implement granular exception handling for products and users

--- src/main/java/com/marketplace/marketplace_api/exception/GlobalExceptionHandler.java
+++ src/main/java/com/marketplace/marketplace_api/exception/GlobalExceptionHandler.java
@@ -6,13 +6,26 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
-@RestController
-public class GlobalExceptionHandler   {
+import java.util.HashMap;
+import java.util.Map;
 
-    @ExceptionHandler (Exception.class)
-    public ResponseEntity<ProductDTO> handleException(Exception e){
-        return new ResponseEntity<>(new ProductDTO(), HttpStatus.INTERNAL_SERVER_ERROR);
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ProductNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleProductNotFound(ProductNotFoundException e) {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", e.getMessage());
+
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleUserNotFound(UserNotFoundException e) {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", e.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 }
